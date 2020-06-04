07ff171af555e9389aacb7e6d7dd9b5ba8dfe96d
Matheus Campos da Silva <silva.campos.matheus@gmail.com>
2020-06-04 00:20:57 -0300

    add 'button' color.

--- app/theme/color.ts
+++ app/theme/color.ts
@@ -49,6 +49,8 @@ export const color = {
    */
   error: palette.angry,
 
+  button: palette.winePurple,
+
   /**
    * Storybook background for Text stories, or any stories where
    * the text color is color.text, which is white by default, and does not show
